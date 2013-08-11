070c755e5b285a7f1c4155cfbd611ceb2a069d9d
Eason Chen <me@easoncxz.com>
2013-08-12 02:36:26 +1200

    Added Report.

--- src/DataTask.sh
+++ src/DataTask.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 cat `ls | grep 'contact'` . | sort | uniq > data.txt
+
+# Although the "." always produces irrelevant output on standard output,
+# it allows this script to be funcitonally correct for the case where
+# there are no files named *.contact in the current directory.
